a6fb5ab6f75d93baad2b4c456fbab5f2d05ec63a
A-Tanner <atanner5@ewu.edu>
2022-05-15 13:46:26 -0700

    Add rotation to target

--- Assets/Scripts/Pathfinding.cs
+++ Assets/Scripts/Pathfinding.cs
@@ -6,6 +6,7 @@ public class Pathfinding : MonoBehaviour
 {
     public GameObject target;
     public float speed;
+    private float offset = -90;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +16,11 @@ public class Pathfinding : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        this.transform.Translate(speed * Time.deltaTime * (target.transform.position - this.transform.position).normalized);
+        this.transform.Translate(Vector3.up * Time.deltaTime * speed);
+        Vector3 targetPoint = target.transform.position - this.transform.position;
+        float angle = Mathf.Atan2(targetPoint.y, targetPoint.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle+offset));
+        //this.transform.Translate(speed * Time.deltaTime * goal);
+
     }
 }
